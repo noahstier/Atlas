48b5cbd4285a94c95298e8e29702ec759598d93a
Noah Stier <noah.stier@gmail.com>
2021-04-14 13:10:44 -0700

    add absorption

--- atlas/model.py
+++ atlas/model.py
@@ -32,56 +32,49 @@ import atlas.transforms as transforms
 from atlas.tsdf import coordinates, TSDF
 
 
-def backproject(voxel_dim, voxel_size, origin, projection, features):
-    """ Takes 2d features and fills them along rays in a 3d volume
+def lightweight_backbone(cfg):
+    import torchvision
+    mobilenet = torchvision.models.mobilenet_v2()
+    output_dim = cfg.MODEL.BACKBONE3D.CHANNELS[0]
+    net = torch.nn.Sequential(
+        mobilenet.features[:2],
+        mobilenet.features[2].conv[:2],
+        torch.nn.Conv2d(96, output_dim, 1, bias=False),
+        torch.nn.BatchNorm2d(output_dim)
+    )
+    return net, 4
 
-    This function implements eqs. 1,2 in https://arxiv.org/pdf/2003.10432.pdf
-    Each pixel in a feature image corresponds to a ray in 3d.
-    We fill all the voxels along the ray with that pixel's features.
 
-    Args:
-        voxel_dim: size of voxel volume to construct (nx,ny,nz)
-        voxel_size: metric size of each voxel (ex: .04m)
-        origin: origin of the voxel volume (xyz position of voxel (0,0,0))
-        projection: bx4x3 projection matrices (intrinsics@extrinsics)
-        features: bxcxhxw  2d feature tensor to be backprojected into 3d
+class DepthDependentConv(torch.nn.Module):
+    def __init__(self, channels, n_depth_bins):
+        super().__init__()
+        n = channels * n_depth_bins
+        self.n_depth_bins = n_depth_bins
+        self.conv1 = torch.nn.Sequential(
+            torch.nn.Conv2d(channels, n, 3, groups=channels, padding=1, bias=False),
+        )
+        self.conv2 = torch.nn.Sequential(
+            torch.nn.Conv2d(n, n, 3, groups=n_depth_bins, padding=1, bias=False),
+            # torch.nn.BatchNorm2d(n),
+            torch.nn.ReLU(),
+            torch.nn.Conv2d(n, n, 3, groups=n_depth_bins, padding=1, bias=False),
+            # torch.nn.BatchNorm2d(n),
+            torch.nn.ReLU(),
+        )
 
-    Returns:
-        volume: b x c x nx x ny x nz 3d feature volume
-        valid:  b x 1 x nx x ny x nz volume.
-                Each voxel contains a 1 if it projects to a pixel
-                and 0 otherwise (not in view frustrum of the camera)
-    """
+    def forward(self, inputs):
+        batch, channels, h, w = inputs.shape
 
-    batch = features.size(0)
-    channels = features.size(1)
-    device = features.device
-    nx, ny, nz = voxel_dim
+        x = self.conv1(inputs)
+        x = x.reshape(batch, channels, self.n_depth_bins, h, w)
+        x = x.transpose(1, 2)
+        x = x.reshape(batch, self.n_depth_bins * channels, h, w)
 
-    coords = coordinates(voxel_dim, device).unsqueeze(0).expand(batch,-1,-1) # bx3xhwd
-    world = coords.type_as(projection) * voxel_size + origin.to(device).unsqueeze(2)
-    world = torch.cat((world, torch.ones_like(world[:,:1]) ), dim=1)
-    
-    camera = torch.bmm(projection, world)
-    px = (camera[:,0,:]/camera[:,2,:]).round().type(torch.long)
-    py = (camera[:,1,:]/camera[:,2,:]).round().type(torch.long)
-    pz = camera[:,2,:]
+        x = self.conv2(x)
+        x = x.reshape(batch, self.n_depth_bins, channels, h, w)
+        x = x.transpose(1, 2)
 
-    # voxels in view frustrum
-    height, width = features.size()[2:]
-    valid = (px >= 0) & (py >= 0) & (px < width) & (py < height) & (pz>0) # bxhwd
-
-    # put features in volume
-    volume = torch.zeros(batch, channels, nx*ny*nz, dtype=features.dtype, 
-                         device=device)
-    for b in range(batch):
-        volume[b,:,valid[b]] = features[b,:,py[b,valid[b]], px[b,valid[b]]]
-
-    volume = volume.view(batch, channels, nx, ny, nz)
-    valid = valid.view(batch, 1, nx, ny, nz)
-
-    return volume, valid
-
+        return x
 
 class VoxelNet(pl.LightningModule):
     """ Network architecture implementing ATLAS (https://arxiv.org/pdf/2003.10432.pdf)"""
@@ -98,9 +91,13 @@ class VoxelNet(pl.LightningModule):
 
         # networks
         self.backbone2d, self.backbone2d_stride = build_backbone2d(cfg)
+        # self.backbone2d, self.backbone2d_stride = lightweight_backbone(cfg)
         self.backbone3d = build_backbone3d(cfg)
+        self.backbone3d_2 = build_backbone3d(cfg)
+
         self.heads2d = PixelHeads(cfg, self.backbone2d_stride)
         self.heads3d = VoxelHeads(cfg)
+        self.heads3d_2 = VoxelHeads(cfg)
 
         # other hparams
         self.pixel_mean = torch.Tensor(cfg.MODEL.PIXEL_MEAN).view(-1, 1, 1)
@@ -141,6 +138,60 @@ class VoxelNet(pl.LightningModule):
         """ Normalizes the RGB images to the input range"""
         return (x - self.pixel_mean.type_as(x)) / self.pixel_std.type_as(x)
 
+    def backproject(self, voxel_dim, voxel_size, origin, projection, features):
+        """ Takes 2d features and fills them along rays in a 3d volume
+    
+        This function implements eqs. 1,2 in https://arxiv.org/pdf/2003.10432.pdf
+        Each pixel in a feature image corresponds to a ray in 3d.
+        We fill all the voxels along the ray with that pixel's features.
+    
+        Args:
+            voxel_dim: size of voxel volume to construct (nx,ny,nz)
+            voxel_size: metric size of each voxel (ex: .04m)
+            origin: origin of the voxel volume (xyz position of voxel (0,0,0))
+            projection: bx4x3 projection matrices (intrinsics@extrinsics)
+            features: bxcxhxw  2d feature tensor to be backprojected into 3d
+    
+        Returns:
+            volume: b x c x nx x ny x nz 3d feature volume
+            valid:  b x 1 x nx x ny x nz volume.
+                    Each voxel contains a 1 if it projects to a pixel
+                    and 0 otherwise (not in view frustrum of the camera)
+        """
+
+        batch, channels, height, width = features.shape
+        device = features.device
+        nx, ny, nz = voxel_dim
+
+        coords = coordinates(voxel_dim, device).unsqueeze(0).expand(batch,-1,-1) # bx3xhwd
+        world = coords.type_as(projection) * voxel_size + origin.to(device).unsqueeze(2)
+        world = torch.cat((world, torch.ones_like(world[:,:1]) ), dim=1)
+
+        camera = torch.bmm(projection, world)
+        px = (camera[:,0,:]/camera[:,2,:]).round().type(torch.long)
+        py = (camera[:,1,:]/camera[:,2,:]).round().type(torch.long)
+        pz = camera[:,2,:]
+
+        z_ind = torch.round((pz - self.near_plane) / (self.far_plane - self.near_plane) * self.n_depth_bins).long()
+        z_ind = torch.clamp(z_ind, 0, self.n_depth_bins - 1)
+
+        # filtered_features = self.depth_dependent_conv(features)
+
+        # voxels in view frustrum
+        valid = (px >= 0) & (py >= 0) & (px < width) & (py < height) & (pz>0) # bxhwd
+
+        # put features in volume
+        volume = torch.zeros(batch, channels, nx*ny*nz, dtype=features.dtype,
+                             device=device)
+        for b in range(batch):
+            volume[b,:,valid[b]] = features[b,:,py[b,valid[b]], px[b,valid[b]]]
+            # volume[b,:,valid[b]] = filtered_features[b, :, z_ind[b, valid[b]], py[b, valid[b]], px[b, valid[b]]]
+
+        volume = volume.view(batch, channels, nx, ny, nz)
+        valid = valid.view(batch, 1, nx, ny, nz)
+
+        return volume, valid
+
     def inference1(self, projection, image=None, feature=None):
         """ Backprojects image features into 3D and accumulates them.
 
@@ -175,7 +226,7 @@ class VoxelNet(pl.LightningModule):
             voxel_dim = self.voxel_dim_train
         else:
             voxel_dim = self.voxel_dim_val
-        volume, valid = backproject(voxel_dim, self.voxel_size, self.origin,
+        volume, valid = self.backproject(voxel_dim, self.voxel_size, self.origin,
                                     projection, feature)
 
         if self.finetune3d:
@@ -239,41 +290,163 @@ class VoxelNet(pl.LightningModule):
         images = image.transpose(0,1)
         projections = projection.transpose(0,1)
 
-        if (not self.batch_backbone2d_time) or (not self.training) or self.finetune3d:
-            # run images through 2d cnn sequentially and backproject and accumulate
-            for image, projection in zip(images, projections):
-                self.inference1(projection, image=image)
+        image = images.reshape(images.shape[0]*images.shape[1], *images.shape[2:])
+        image = self.normalizer(image)
+        features = self.backbone2d(image)
 
+        # run 2d heads
+        if targets2d is not None:
+            targets2d = {
+                key: value.transpose(0,1).view(
+                    images.shape[0]*images.shape[1], *value.shape[2:])
+                for key, value in targets2d.items()}
+        outputs2d, losses2d = self.heads2d(features, targets2d)
+
+        # reshape back
+        features = features.view(images.shape[0],
+                                 images.shape[1],
+                                 *features.shape[1:])
+        outputs2d = {
+            key:value.transpose(0,1).reshape(
+                images.shape[0], images.shape[1], *value.shape[1:]) 
+            for key, value in outputs2d.items()}
+
+
+        # for projection, feature in zip(projections, features):
+        #     self.inference1(projection, feature=feature)
+
+        # # run 3d cnn
+        # outputs3d, losses3d = self.inference2(targets3d)
+
+        if self.training:
+            voxel_dim = self.voxel_dim_train
         else:
-            # run all images through 2d cnn together to share batchnorm stats
-            image = images.reshape(images.shape[0]*images.shape[1], *images.shape[2:])
-            image = self.normalizer(image)
-            features = self.backbone2d(image)
+            voxel_dim = self.voxel_dim_val
 
-            # run 2d heads
-            if targets2d is not None:
-                targets2d = {
-                    key: value.transpose(0,1).view(
-                        images.shape[0]*images.shape[1], *value.shape[2:])
-                    for key, value in targets2d.items()}
-            outputs2d, losses2d = self.heads2d(features, targets2d)
+        _, batch_size, _, featheight, featwidth = features.shape
 
-            # reshape back
-            features = features.view(images.shape[0],
-                                     images.shape[1],
-                                     *features.shape[1:])
-            outputs2d = {
-                key:value.transpose(0,1).reshape(
-                    images.shape[0], images.shape[1], *value.shape[1:]) 
-                for key, value in outputs2d.items()}
+        tsdf_estimate = torch.zeros((batch_size, *voxel_dim), device=features.device, dtype=features.dtype)
 
-            for projection, feature in zip(projections, features):
-                self.inference1(projection, feature=feature)
 
-        # run 3d cnn
-        outputs3d, losses3d = self.inference2(targets3d)
+        for it in range(2):
+            density_estimate = 1 / (1 + torch.exp(-4 * tsdf_estimate))
+            density_estimate = density_estimate.to(tsdf_estimate.dtype)
+
+
+            coords = coordinates(voxel_dim, features.device).unsqueeze(0).expand(batch_size,-1,-1)
+            world_voxels = coords.type_as(features) * self.voxel_size + self.origin.to(features.device).unsqueeze(2)
+            world_voxels_h = torch.cat((world_voxels, torch.ones_like(world_voxels[:,:1]) ), dim=1)
+
+            volume = torch.zeros((batch_size, features.shape[2], *voxel_dim), device=tsdf_estimate.device, dtype=tsdf_estimate.dtype)
+            weights = torch.zeros((batch_size, *voxel_dim), device=tsdf_estimate.device, dtype=tsdf_estimate.dtype)
+
+            for img_ind in range(features.shape[0]):
+                projection = projections[img_ind].clone().to(world_voxels_h.dtype)
+                projection[:,:2,:] = projection[:,:2,:] / self.backbone2d_stride
+
+                camera = torch.bmm(projection, world_voxels_h)
+                px = torch.round(camera[:, 0, :] / camera[:, 2, :]).long()
+                py = torch.round(camera[:, 1, :] / camera[:, 2, :]).long()
+                pz = camera[:,2,:]
+                in_frustum = (pz > 0) & (px >= 0) & (py >= 0) & (px < featwidth - 1) & (py < featheight - 1)
+
+                pose = batch['pose'][:, img_ind]
+
+                for b in range(batch_size):
+                    if not torch.any(in_frustum[b]):
+                        continue
+
+                    absorption = self.get_absorption(pose[b], world_voxels[b, :, in_frustum[b]], density_estimate[b])
+
+                    '''
+                    import ipdb
+                    ipdb.set_trace()
+
+                    import open3d as o3d
+                    import matplotlib.pyplot as plt
+                    import numpy as np
+                    cam_pcd = o3d.geometry.PointCloud(o3d.utility.Vector3dVector(batch['pose'][b, :, :3, 3].cpu()))
+                    # in_vox = world_voxels[b].transpose(0, 1)[in_frustum[b]].cpu().float().numpy()
+                    # out_vox = world_voxels[b].transpose(0, 1)[~in_frustum[b]].cpu().float().numpy()
+                    # out_vox = world_voxels[b].transpose(0, 1)[~in_frustum[b]].cpu().float().numpy()
+                    vox_pcd = o3d.geometry.PointCloud(o3d.utility.Vector3dVector(world_voxels[b].transpose(0, 1).cpu().float().numpy()))
+                    # in_vox_pcd = o3d.geometry.PointCloud(o3d.utility.Vector3dVector(in_vox))
+                    # out_vox_pcd = o3d.geometry.PointCloud(o3d.utility.Vector3dVector(out_vox))
+                    # in_vox_pcd.paint_uniform_color(np.array([1, 0, 0], dtype=float))
+                    # out_vox_pcd.paint_uniform_color(np.array([0, 0, 1], dtype=float))
+                    colors = plt.cm.jet(in_frustum[b].cpu().float().numpy())[:, :3]
+                    vox_pcd.colors = o3d.utility.Vector3dVector(colors)
+                    o3d.visualization.draw_geometries([cam_pcd, vox_pcd])
+                    '''
+
+                    f = features[img_ind, b, :, py[b, in_frustum[b]], px[b, in_frustum[b]]]
+
+                    volume[b, :, in_frustum[b].reshape(voxel_dim)] += f * absorption[None]
+                    weights[b, in_frustum[b].reshape(voxel_dim)] += absorption
+
+            if it == 0:
+                x = self.backbone3d(volume)
+                outputs3d, losses3d = self.heads3d(x, targets3d)
+                tsdf_estimate = outputs3d['vol_04_tsdf'][:, 0]
+                losses3d = {k + '_1': v for k, v in losses3d.items()}
+            elif it == 1:
+                x = self.backbone3d_2(volume)
+                _outputs3d, _losses3d = self.heads3d_2(x, targets3d)
+                for k, v in _losses3d.items():
+                    losses3d[k] = v
 
         return {**outputs2d, **outputs3d}, {**losses2d, **losses3d}
+
+    def get_absorption(self, pose, voxel_centers, density):
+
+        voxel_centers = voxel_centers.transpose(0, 1)
+    
+        # get camera rays
+        ray_origin = pose[:3, 3]
+        ray_direction = voxel_centers - ray_origin[None, :]
+        ray_length = torch.norm(ray_direction, dim=-1)
+        ray_direction /= ray_length[:, None]
+        
+        # sample n points along each ray
+        n = 16
+        dist = torch.linspace(0, 1, n, device=pose.device) + torch.rand(n, device=pose.device) / (n - 1)
+        dist = torch.clamp(dist, 0, 1)
+        pts = ray_origin[None, None] + ray_length[None, :, None] * ray_direction[None] * dist[:, None, None] 
+
+        # get volume density at each point
+        
+        inds = (pts - self.origin[:, None].to(pts.device)) / self.voxel_size
+        inds = inds.reshape(-1, 3).round().long()
+
+        '''
+        v = voxel_centers.cpu().numpy()
+        import open3d as o3d
+        vox_pcd = o3d.geometry.PointCloud(o3d.utility.Vector3dVector(v))
+        pcd = o3d.geometry.PointCloud(o3d.utility.Vector3dVector(pts.cpu().reshape(-1, 3)))
+        o3d.visualization.draw_geometries([vox_pcd, pcd])
+        '''
+
+        shape = torch.Tensor(list(density.shape)).long().to(density.device)
+        valid = torch.all((inds >= 0) & (inds < shape), dim=-1)
+
+        sigma = torch.empty(inds.shape[0], device=inds.device, dtype=density.dtype)
+        sigma[valid] = density[inds[valid, 0], inds[valid, 1], inds[valid, 2]]
+        sigma[~valid] = 0
+        sigma = sigma.reshape(n, -1)
+
+        # integrate
+
+        step = torch.empty((n, pts.shape[1]), device=pts.device)
+    
+        step_vec = pts[1:] - pts[:-1]
+        step[1:] = (step_vec[..., 0] ** 2 + step_vec[..., 1] ** 2 + step_vec[..., 2] ** 2) ** .5
+        step_vec = pts[0] - ray_origin
+        step[0] = (step_vec[..., 0] ** 2 + step_vec[..., 1] ** 2 + step_vec[..., 2] ** 2) ** .5
+
+        ray = torch.exp(-torch.sum(sigma * step, dim=0)).type_as(density)
+        absorption = ray * sigma[-1]
+    
+        return absorption
 
 
     def postprocess(self, batch):
